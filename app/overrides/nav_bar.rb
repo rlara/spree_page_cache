935d1d25bc7fa55126b9cec9755635c9e6d511ff
acolin <acolin@incaztech.com>
2013-09-05 14:12:01 -0500

    changed styles for login bar and account bar to bebitos

--- app/overrides/nav_bar.rb
+++ app/overrides/nav_bar.rb
@@ -1,8 +1,14 @@
 # we cancel the spree_auth override by reusing the override name
 Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
                      :name => "auth_shared_login_bar",
-                     :insert_before => "li#search-bar",
+                     :replace => "[data-hook='login-bar']",
                      :partial => "spree/shared/auth_bar",
+                     :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
+                     :name => "auth_shared_account_bar",
+                     :replace => "[data-hook='account-bar']",
+                     :partial => "spree/shared/account_bar",
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/shared/_store_menu",
