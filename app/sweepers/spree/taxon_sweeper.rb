c2aadbacfb4f3eabe504c47c257a674727617ba2
acolin <acolin@incaztech.com>
2013-09-10 14:39:04 -0500

    expire taxons fragments when updating taxons

--- app/sweepers/spree/taxon_sweeper.rb
+++ app/sweepers/spree/taxon_sweeper.rb
@@ -6,11 +6,9 @@ module Spree
       expire_page "/"
       expire_page products_path
       expire_action(:controller => 'products', :action => :index)
-      expire_action(:controller => 'taxons', :action => :show)
       expire_fragment('taxon_items_header')
       expire_fragment('taxon_items_footer')
-      FileUtils.rm_rf "#{page_cache_directory}/t"
-      FileUtils.rm_rf "#{page_cache_directory}/taxons"
+      expire_fragment(%r{taxons/*})
       FileUtils.rm_rf "#{page_cache_directory}/index.html"
     end
   end
