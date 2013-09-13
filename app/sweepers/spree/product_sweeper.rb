5d535c9bf5f2cf427039e1fdc99095fc4845dc68
acolin <acolin@incaztech.com>
2013-09-12 20:11:11 -0500

    added changes to sweepers expiring params

--- app/sweepers/spree/product_sweeper.rb
+++ app/sweepers/spree/product_sweeper.rb
@@ -6,11 +6,11 @@ module Spree
       expire_page product_path(product)
       expire_page products_path
       expire_page "/"
-      expire_action(:controller => 'products', :action => :index)
-      expire_fragment(%r{taxons/*})
+      expire_fragment(%r{t/*})
       expire_fragment(%r{products.*})
 
       FileUtils.rm_rf "#{page_cache_directory}/index.html"
+      FileUtils.rm_rf "#{page_cache_directory}/t"
     end
   end
 end
--- app/sweepers/spree/taxon_sweeper.rb
+++ app/sweepers/spree/taxon_sweeper.rb
@@ -6,10 +6,8 @@ module Spree
       expire_page "/"
       expire_page products_path
       expire_fragment(%r{products.*})
-      expire_action(:controller => 'products', :action => :index)
-      expire_fragment('taxon_items_header')
-      expire_fragment('taxon_items_footer')
-      expire_fragment(%r{taxons/*})
+      expire_fragment(%r{t/*})
+      FileUtils.rm_rf "#{page_cache_directory}/t"
       FileUtils.rm_rf "#{page_cache_directory}/index.html"
     end
   end
